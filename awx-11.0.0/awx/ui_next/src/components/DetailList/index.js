4692e86d5587e275a2c2e8c067054e90681c77fb
root <root@controller.ibmtraining.com>
2020-12-08 11:56:47 +0000

    third

--- awx-11.0.0/awx/ui_next/src/components/DetailList/index.js
+++
@@ -1,3 +0,0 @@
-export { default as DetailList } from './DetailList';
-export { default as Detail, DetailName, DetailValue } from './Detail';
-export { default as UserDateDetail } from './UserDateDetail';
--- awx-11.0.0/awx/ui_next/src/components/PromptDetail/PromptWFJobTemplateDetail.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { CardBody } from '@components/Card';
-
-function PromptWFJobTemplateDetail() {
-  return <CardBody>Coming soon :)</CardBody>;
-}
-
-export default PromptWFJobTemplateDetail;
